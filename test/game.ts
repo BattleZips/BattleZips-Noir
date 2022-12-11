ee300d0a980ae01f1417369c908b868fe1a81b15
Ian-Bright <ianbrighton1@gmail.com>
2022-12-11 16:21:36 -0700

    clean up comments in game test

--- test/game.ts
+++ test/game.ts
@@ -10,52 +10,63 @@ import { resolve } from 'path';
 import { boards, initialize, printLog, shots } from './utils';
 
 describe('Play entire BattleZip game', async () => {
-    let alice, bob, operator; // players
-    let barretenberg;
-    let boardAcir, boardProver, boardVerifier;
-    let boardHashes // store hashed board for alice and bob
-    let game; // contracts
-    let shotAcir, shotProver, shotVerifier;
+    let alice, bob, operator; // Players
+    let barretenberg; // Instance of Aztec Barretenberg
+    let boardAcir, boardProver, boardVerifier; // Materials related to proving and verifying board proof
+    let boardHashes // Store hashed board for alice and bob
+    let game; // Battleship Game contract
+    let shotAcir, shotProver, shotVerifier; // Materials related to proving and verifying shot proof
 
+    /**
+     * Helper function to carry out turns in the Battleship game
+     * 
+     * @param aliceNonce Representation of what the current turn in the game is
+     */
     async function simulateTurn(aliceNonce) {
         printLog(`Bob reporting result of Alice shot #${aliceNonce - 1} (Turn ${aliceNonce * 2 - 1})`)
 
-
+        // Create shot input for shot proof
         let abi = {
             hash: boardHashes.bob,
             hit: 1,
             ships: boards.bob,
             shot: shots.alice[aliceNonce - 1],
         }
-        // compute witness and run through groth16 circuit for proof / signals
+
+        // Create proof of shot
         let proof = await create_proof(shotProver, shotAcir, abi);
-        // verify proof locally
+        // Verify shot proof locally
         await verify_proof(shotVerifier, proof);
-        // prove alice's registered shot hit, and register bob's next shot
+
+        // Prove alice's registered shot hit, and register bob's next shot
         await (await game.connect(bob).turn(
-            1, // game id
-            true, // hit bool
-            shots.bob[aliceNonce - 1], // returning fire / next shot to register (not part of proof)
+            1, // Game id
+            true, // Hit bool
+            shots.bob[aliceNonce - 1], // Returning fire / next shot to register (not part of proof)
             proof
         )).wait()
-        /// ALICE PROVES BOB PREV REGISTERED SHOT MISSED ///
+
+        /// Shot is proved to be a miss ///
         printLog(`Alice reporting result of Bob shot #${aliceNonce - 1} (Turn ${aliceNonce * 2})`)
-        // bob's shot hit/miss integrity proof public / private inputs
+
+        // Bob's shot proof abi
         abi = {
             hash: boardHashes.alice,
             hit: 0,
             ships: boards.alice,
             shot: shots.bob[aliceNonce - 1],
         };
-        // compute witness and run through groth16 circuit for proof / signals
+
+        // Create proof for Bob's shot
         proof = await create_proof(shotProver, shotAcir, abi);
+        // Verify proof of Bob's shot locally
         await verify_proof(shotVerifier, proof);
-        // verify proof locally
-        // prove bob's registered shot missed, and register alice's next shot
+
+        // Prove Bob's registered shot missed, and register alice's next shot
         await (await game.connect(alice).turn(
-            1, // game id
-            false, // hit bool
-            shots.alice[aliceNonce], // returning fire / next shot to register (not part of proof)
+            1, // Game id
+            false, // Hit bool
+            shots.alice[aliceNonce], // Returning fire / next shot to register (not part of proof)
             proof
         )).wait()
     }
@@ -64,13 +75,17 @@ describe('Play entire BattleZip game', async () => {
 
         // Initialize barretenberg
         barretenberg = await BarretenbergWasm.new();
-        // set players
+
+        // Set players
         const signers = await ethers.getSigners()
         operator = signers[0];
         alice = signers[1];
         bob = signers[2];
-        // initialize and store 
+
+        // Initialize contracts and Aztec backend
         ({ boardHashes, game, } = await initialize(barretenberg, ethers.constants.AddressZero))
+
+        // Create prover and verifier for board and shot proofs
         boardAcir = compile(resolve(__dirname, '../circuits/board/src/main.nr')).circuit;
         [boardProver, boardVerifier] = await setup_generic_prover_and_verifier(boardAcir);
         shotAcir = compile(resolve(__dirname, '../circuits/shot/src/main.nr')).circuit;
@@ -80,35 +95,37 @@ describe('Play entire BattleZip game', async () => {
     describe("Play game to completion", async () => {
         it("Start a new game", async () => {
 
-            // board starting verification proof public / private inputs
+            // Create board inputs for Alice's board proof
             const abi = {
                 hash: boardHashes.alice,
                 ships: boards.alice,
             };
 
-            // Compute witness and generate proof
+            // Generate board proof for Alice
             const proof = await create_proof(boardProver, boardAcir, abi);
+            // Verify board proof locally
             await verify_proof(boardVerifier, proof);
 
             console.log('Starting new game...');
+            // Create new Battleship Game with Alice's board proof
             await (await game.connect(alice).newGame(boardHashes.alice, proof)).wait();
             console.log('New Battleship game started.');
         });
 
         it("Join an existing game", async () => {
-            // board starting verification proof public / private inputs
+            // Create board inputs for Bob's board proof
             const abi = {
                 hash: boardHashes.bob,
                 ships: boards.bob,
             }
-            // compute witness and run through groth16 circuit for proof / signals
+            // Compute witness and run through groth16 circuit for proof / signals
             const proof = await create_proof(boardProver, boardAcir, abi);
 
-            // verify proof locally
+            // Verify proof locally
             await verify_proof(boardVerifier, proof);
 
 
-            // prove on-chain hash is of valid board configuration
+            // Prove on-chain hash is of valid board configuration for Bob
             await (await game.connect(bob).joinGame(
                 1,
                 boardHashes.bob,
@@ -117,32 +134,37 @@ describe('Play entire BattleZip game', async () => {
         });
 
         it("opening shot", async () => {
+            // Alice takes first turn in game with opening shot. No proof needed
             await (await game.connect(alice).firstTurn(1, [1, 0])).wait()
         });
 
         it('Prove hit/ miss for 32 turns', async () => {
+            // Play out game with a move made for Alice and Bob each loop iteration
             for (let i = 1; i <= 16; i++) {
                 await simulateTurn(i)
             }
         });
 
         it('Alice wins on sinking all of Bob\'s ships', async () => {
-            // bob's shot hit/miss integrity proof public / private inputs
+
+            // Bob's shot hit/miss integrity proof public / private inputs
             const abi = {
                 hash: boardHashes.bob,
                 hit: 1,
                 ships: boards.bob,
                 shot: shots.alice[16],
             }
-            // compute witness and run through groth16 circuit for proof / signals
+            // Create proof for final shot in game
             const proof = await create_proof(shotProver, shotAcir, abi);
-            // verify proof locally
+
+            // Verify proof locally
             await verify_proof(shotVerifier, proof);
-            // prove alice's registered shot hit, and register bob's next shot
+
+            // Prove Alice's registered shot hit, and register Bob's next shot
             await (await game.connect(bob).turn(
-                1, // game id
-                true, // hit bool
-                [0, 0], // shot params are ignored on reporting all ships sunk, can be any uint256
+                1, // Game id
+                true, // Hit bool
+                [0, 0], // Shot params are ignored on reporting all ships sunk, can be any uint256
                 proof
             )).wait()
         })
